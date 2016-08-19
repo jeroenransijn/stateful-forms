ea543bdf87d5f262c97f059a746c42f90cf37f32
Jeroen Ransijn <jssrdesign@gmail.com>
2016-08-19 12:48:34 -0700

    build setup upated

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,45 @@
+'use strict';
+
 var browserify = require('browserify');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+function b () {
+  return browserify({
+    entries: './src/stateful-forms.js',
+    debug: true
+  });
+}
 
 gulp.task('build', function() {
-	console.log('build');
-	return browserify({
-			entries: './src/stateful-forms.js',
-			debug: true
-		})
-		.bundle()
-		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
-		//Pass desired output filename to vinyl-source-stream
-		.pipe(source('stateful-forms.js'))
-		// Start piping stream to tasks!
-		.pipe(gulp.dest('./dist/'));
+  return b().bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('stateful-forms.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-min', function() {
+  return b().bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('stateful-forms.min.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
 });
 
 
 gulp.task('watch', function () {
-	gulp.watch('./src/**/*', ['build']);
+  gulp.watch('./src/**/*', ['build']);
 });
